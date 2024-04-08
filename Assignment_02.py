005338d42dc509dc7628e2ca91b4bb8534a3f1b2
Muqaddas Ali <157135717+MuqsAli@users.noreply.github.com>
2024-04-08 15:06:27 +0500

    Assignment 02

---
+++ Assignment_02.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Task 01:
+# 
+# Print Your Name with your Father name and Date of birth using suitable escape sequence charactor
+# 
+# 
+
+# In[1]:
+
+
+print("Name: Muqaddas Ali \nFather Name: Sabir Ali \nD.O.B: 17/12/2000")
+
+
+# # Task 02:
+# 
+# Write your small bio using variables and print it using print function
+
+# In[4]:
+
+
+Name = "Muqaddas Ali"
+Age = 23
+Occupation = "Student"
+Field_of_interest = "Data Science and Graphic Designing"
+
+print("Name: ", Name)
+print("Age: ", Age)
+print("Occupation: ", Occupation)
+print("Field_of_interest: ", Field_of_interest)
+
+
+# # Task 03:
+# 
+# Write a program in which use all the operators we can use in Python
+
+# In[ ]:
+
+
+a = 10
+b = 5
+print("Arithmetic Operators:")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
+
+# # Task 04:
+# 
+# 
+# 1. Mention Marks of English , Islamiat and Maths out of 100 in 3 different variables
+# 
+# 2. Mention Variable of Total Marks and assign 300 to it
+# 
+# 3. Calculate Percentage
+
+# In[5]:
+
+
+english_marks = 85
+islamiat_marks = 90
+maths_marks = 95
+obtained_marks = english_marks + islamiat_marks + maths_marks
+total_marks = 300
+
+print("English Marks= ", english_marks)
+print("Islamiat Marks= ", islamiat_marks)
+print("Maths Marks= ", maths_marks)
+print("Total Marks= ", total_marks)
+print("Obtained Marks= ", obtained_marks)
+
+
+percentage = (obtained_marks / total_marks) * 100
+
+print("Percentage:", percentage)
+
+
+# In[ ]:
+
+
+
+
